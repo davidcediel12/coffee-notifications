dbfde1590884eb61196f6196f3f6d76cd19bfcea
David Cediel <esdago18@gmail.com>
2025-05-08 00:47:36 +0200

    Add server side event to notify new elements

--- src/main/java/com/cordilleracoffee/appnotification/infrastructure/api/controller/AppNotificationController.java
+++ src/main/java/com/cordilleracoffee/appnotification/infrastructure/api/controller/AppNotificationController.java
@@ -1,6 +1,9 @@
 package com.cordilleracoffee.appnotification.infrastructure.api.controller;
 
 
+import com.cordilleracoffee.appnotification.infrastructure.messaging.consumer.TopicListener;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,11 +14,26 @@ import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/v1/app-notification")
+@RequiredArgsConstructor
+@Slf4j
 public class AppNotificationController {
 
+    private final TopicListener topicListener;
 
     @PostMapping(value = "/register", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ServerSentEvent<String>> suscribeToEvents(@RequestHeader("App-User-ID") String userId){
-        return null;
+        return topicListener.getEvents()
+                .doFirst(() -> log.info("User {} is subscribing to events", userId))
+                .doOnNext(event -> log.info("New event received: {}", event))
+                .map(event -> ServerSentEvent.<String>builder()
+                        .data(event)
+                        .event("New notification")
+                        .build());
+    }
+
+    @PostMapping("/emit")
+    public void emit(){
+        topicListener.emit("Hello from App Notification");
+        log.info("Event emitted");
     }
 }
--- src/main/java/com/cordilleracoffee/appnotification/infrastructure/messaging/consumer/TopicListener.java
+++ src/main/java/com/cordilleracoffee/appnotification/infrastructure/messaging/consumer/TopicListener.java
@@ -1,23 +1,62 @@
 package com.cordilleracoffee.appnotification.infrastructure.messaging.consumer;
 
 
-import lombok.RequiredArgsConstructor;
+import com.cordilleracoffee.appnotification.infrastructure.messaging.consumer.dto.CheckoutEventType;
+import com.cordilleracoffee.appnotification.infrastructure.messaging.consumer.dto.CheckoutMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
-import reactor.kafka.receiver.KafkaReceiver;
-import reactor.kafka.receiver.ReceiverOptions;
-
-import java.util.List;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
 
 @Component
-@RequiredArgsConstructor
+@Slf4j
 public class TopicListener {
 
-    private final KafkaReceiver<String, String> checkoutReceiver;
+    private final ObjectMapper objectMapper;
+    private final Sinks.Many<String> eventSink;
 
-    public TopicListener(ReceiverOptions<String, String> receiverOptions) {
+    public TopicListener(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.eventSink = Sinks.many().multicast().directBestEffort();
+    }
 
-        receiverOptions.subscription(List.of("checkout"));
-        receiverOptions.
-        this.checkoutReceiver = KafkaReceiver.create();
+    @KafkaListener(groupId = "app-notifications", topics = "checkout")
+    public void receiveCheckout(GenericMessage<String> message) {
+
+        log.info("Received Message from checkout topic: {}", message.getPayload());
+        CheckoutMessage checkoutMessage;
+
+        try {
+            checkoutMessage = objectMapper.readValue(message.getPayload(), CheckoutMessage.class);
+
+            CheckoutEventType eventType = checkoutMessage.checkoutEventType();
+
+            if (eventType == null) {
+                return;
+            }
+
+            Sinks.EmitResult emitResult = eventSink.tryEmitNext(checkoutMessage.content().toString());
+
+            if (emitResult.isSuccess()) {
+                log.info("Event sink emitted: {}", checkoutMessage.content().asText());
+            } else {
+                log.error("Event sink emission failed");
+            }
+        } catch (JsonProcessingException e) {
+            log.error("Unable to parse message", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void emit(String message) {
+        eventSink.tryEmitNext(message);
+    }
+
+    public Flux<String> getEvents() {
+        return eventSink.asFlux();
     }
 }
